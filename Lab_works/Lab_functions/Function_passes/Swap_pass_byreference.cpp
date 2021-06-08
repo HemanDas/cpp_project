39d60c5b5aa98be38ee1f1b08365028cb347df1d
HemanDas <hemandas630@gmail.com>
2021-06-08 14:22:33 +0545

    swap program using pass by value

---
+++ Lab_works/Lab_functions/Function_passes/Swap_pass_byreference.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+void swap(int &a, int &b) 
+{
+    int c;
+    c = a;
+    a = b;
+    b = c;
+}
+
+int main()
+{
+    int a = 1, b = 2;
+
+    cout << "Before swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    swap(a, b);
+
+    cout << "After swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
