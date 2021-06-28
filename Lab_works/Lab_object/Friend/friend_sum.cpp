a3806699474e65b1be432c2c460134ccf2d65d83
HemanDas <hemandas630@gmail.com>
2021-06-28 14:57:23 +0545

    sum of two private member of different class

---
+++ Lab_works/Lab_object/Friend/friend_sum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class group1{
+    private:
+        float num_1;
+    public:
+        friend class group2;
+
+        void getdata()
+       {
+           cout<<"Enter The first Number :"<<endl;
+           cin>>num_1;
+       }
+};
+class group2{
+    private:
+        float num_2;
+    public:
+        void getdata()
+       {
+           cout<<"Enter The second Number :"<<endl;
+           cin>>num_2;
+       }
+        float mean( group1 S1,group2 S2 ) 
+      { 
+             return (S1.num_1 +S2.num_2);
+      }
+};
+int main(){
+    group1 a;
+    group2 b,c;
+    a.getdata();
+    b.getdata();
+    cout<<"Addition of both Entered numbers is :"<<c.mean(a,b)<<endl;
+
+}
